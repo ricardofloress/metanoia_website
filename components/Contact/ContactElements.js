3ce626adfafbce2f0872fab6240ef3cd5ab88d79
Ricardo Flores <ricardo.flores11@hotmail.com>
2021-03-11 19:28:56 +0000

    Resizing 4

--- components/Contact/ContactElements.js
+++ components/Contact/ContactElements.js
@@ -2,7 +2,7 @@
 import styled from "styled-components";
 
 export const ContactContainer = styled.div`
-    height: 500px;
+    height: 400px;
     margin-top: 80px;
     width: 100%;
     display: flex;
